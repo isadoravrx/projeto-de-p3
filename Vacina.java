31a93aac2f9d5c5d41f6cbab7bb5710b449ef0c1
Gabriel Tôrres <gabrieltorres2110@gmail.com>
2023-04-27 11:52:42 -0300

    adolescentes

--- Vacina.java
+++ Vacina.java
@@ -2,18 +2,18 @@ public class Vacina{
 
     boolean[] vacinas;
     String[] vacinas_crianca = {"bcg", "hepatite b", "poliomielite", "vrh", "penta", "pcv 10", "meningocócica c", "febre amarela", "sarampo, caxumba e rubéola", "sarampo, caxumba, rubéola e varicela", "hepatite a", "difteria e tétano", "hpv", "ppv 23", "varicela"};
-    String[] vacinas_adolescente;
+    String[] vacinas_adolescente = {"hepatite b", "difteria e tétano", "febre amarela", "sarampo, caxumba e rubéola", "hpv", "ppv 23", "meningocócica"};
     String[] vacinas_adulto;
     int[] numero_de_doses_crianca = {1,1,3,2,3,2,2,1,2,2,1,1,3,2,1,1} ;
-    int[] numero_de_doses_adolescentes;
+    int[] numero_de_doses_adolescentes = {3, 3, 1, 2, 2, 1, 1};
     int[] numero_de_doses_adultos;
 
     int[] numero_de_doses_reforco_crianca = {0,0,2,0,2,1,1,1,0,0,0,2,1,8,0,0,0} ;
-    int[] numero_de_doses_reforco_adolescentes;
+    int[] numero_de_doses_reforco_adolescentes = {0, 8, 0, 0, 0 , 0, 0};
     int[] numero_de_doses_reforco_adultos;
 
     int[] intervalo_entre_doses_crianca = {0,0,30,30,30,60,60,30,30,30,0,180,30,0,0,30};
-    int[] intervalo_entre_doses_adolescentes;
+    int[] intervalo_entre_doses_adolescentes = {180, 60, 0, 30, 180, 0, 0};
     int[] intervalo_entre_doses_adultos;
     
     // todas vacinas
