743710e7d0a58668bcb01c7d6f73c0c068a9b785
isadoravrx <isavitoriaxavier@gmail.com>
2023-04-27 11:43:36 -0300

    colocando vacinas criancas

--- Vacina.java
+++ Vacina.java
@@ -1,4 +1,17 @@
 public class Vacina{
+
+    boolean[] vacinas;
+    String[] vacinas_crianca = {"bcg", "hepatite b", "poliomielite", "vrh", "penta", "pcv 10", "meningocócica c", "febre amarela", "sarampo, caxumba e rubéola", "sarampo, caxumba, rubéola e varicela", "hepatite a", "difteria e tétano", "hpv", "ppv 23", "varicela"};
+    String[] vacinas_adolescente;
+    String[] vacinas_adulto;
+    int[] numero_de_doses_crianca = {1,1,3,2,3,2,2,1,2,2,1,1,3,2,1,1} ;
+    int[] numero_de_doses_adolescentes;
+    int[] numero_de_doses_adultos;
+
+    int[] numero_de_doses_reforco_crianca = {0,0,2,0,2,1,1,1,0,0,0,2,1,8,0,0,0} ;
+    int[] numero_de_doses_reforco_adolescentes;
+    int[] numero_de_doses_reforco_adultos;
+    
     // todas vacinas
     // quantidade de doses 
     // prazo entre as doses
