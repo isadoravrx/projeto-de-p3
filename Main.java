07d5775752b9404d8c94400f26b5bb2b7ec6d21e
isadoravrx <isavitoriaxavier@gmail.com>
2023-04-30 14:26:38 -0300

    trabalho em andamento

--- Main.java
+++ Main.java
@@ -1,10 +1,45 @@
 import java.util.Scanner;
-    
+import javax.swing.*;
+import java.awt.*;
+import java.util.Calendar;
+
 public class Main{
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    
-       
+        String nome;
+        String cpf;
+        int dia;
+        int mes;
+        int ano;
+
+        String[] opcoes = {};
+
+        System.out.println("Qual é o seu nome?");
+        nome = sc.nextLine();
+        System.out.println("Qual é o seu cpf?");
+        cpf = sc.nextLine();
+        System.out.println("Qual é o dia do seu nascimento?");
+        dia = sc.nextInt();
+        System.out.println("Qual é o mês do seu nascimento?");
+        mes = sc.nextInt();
+        System.out.println("Qual é o ano do seu nascimento?");
+        ano = sc.nextInt();
+
+        int idade;
+        Calendar hoje = Calendar.getInstance();
+        int anodehoje = hoje.get(Calendar.YEAR);
+        int mesdehoje = hoje.get(Calendar.MONTH);
+        int diadehoje = hoje.get(Calendar.DAY_OF_MONTH);
+        
+        idade = anodehoje - ano;
+        if(mesdehoje < mes){
+            idade--;
+        }else if(diadehoje < dia && mesdehoje == mes){
+            idade--;
+        }
+        
+        System.out.println(idade);
+
     }
 }
